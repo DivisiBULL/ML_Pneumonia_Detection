d48979f8a7d3c7d102422e50dd2db5ec9c6d4481
DivisiBULL <64914900+DivisiBULL@users.noreply.github.com>
2023-03-09 10:41:10 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.write('Hello World!')
+from transformers import pipeline
+
+pipe = pipeline('sentiment-analysis')
+text = st.text_area('enter some text')
+
+if text:
+  out = pipe(text)
+  st.json(out)
